4b87fc7ff103825ed6ea4c71ff4ceec2b0ba07ba
vaba1010 <valentin.baranov@netcracker.com>
2018-02-16 15:29:22 +0400

    Particle model added

--- src/main/java/particles/Particle.java
+++ src/main/java/particles/Particle.java
@@ -1,18 +1,31 @@
 package particles;
 
+import attribute.Scalar;
+import attribute.Vector;
+
 /**
  * Created by PrifizGamer on 16.02.2018.
  */
 public class Particle {
-        double m;
-        double x;
-        double y;
-        double z;
-        double vx;
-        double vy;
-        double vz;
-        double Ix;
-        double Iy;
-        double Iz;
-        double limitF;
+
+    private Scalar mass;
+    private Vector coordinates;
+    private Vector velocities;
+    private Vector impmomemts;
+    private Scalar forceLimit;
+
+    // compare upper /\ versus down \/
+
+
+//        double m;
+//        double x;
+//        double y;
+//        double z;
+//        double vx;
+//        double vy;
+//        double vz;
+//        double Ix;
+//        double Iy;
+//        double Iz;
+//        double limitF;
 }

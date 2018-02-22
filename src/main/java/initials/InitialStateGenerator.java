3c40b2eee23ffa8872f9356dbe5cc55d53432f08
Prifiz <valyok.baranov@gmail.com>
2018-02-23 00:50:04 +0400

    Unused code cleanup

--- src/main/java/initials/InitialStateGenerator.java
+++
@@ -1,7 +0,0 @@
-package initials;
-
-public interface InitialStateGenerator {
-    NBodyConfiguration generateCoordinates(int partsCount);
-    NBodyConfiguration generateVelocities(int partsCount);
-    NBodyConfiguration generateMasses(int partsCount);
-}

90e075b83992ee61631772b0d3451869612c8487
vaba1010 <valentin.baranov@netcracker.com>
2018-04-27 11:14:28 +0400

    Samples added for tests

--- initial-conditions-client/src/main/java/Main.java
+++ initial-conditions-client/src/main/java/Main.java
@@ -11,7 +11,9 @@ import particles.Particle;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
